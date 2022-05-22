71caec08443503679f6eb04bb4dd24f5eddc9273
Miguel Zambrano <103617707+Marytieera79@users.noreply.github.com>
2022-05-22 16:10:56 -0500

    feat: agregando 10 archivos

---
+++ bravo/1.js
@@ -0,0 +1,11 @@
+let ejercicio = [100, 200, 300, 400, 500]
+
+console.log(ejercicio)
+
+ejercicio.shift()
+ejercicio.shift()
+ejercicio.shift()
+ejercicio.shift()
+ejercicio.shift()
+
+console.log(ejercicio)
